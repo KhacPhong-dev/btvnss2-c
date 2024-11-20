45baa67e1c58b81d87fe5ae3bd66bb0997b87aac
KhacPhong-dev <kaito0676@gmail.com>
2024-11-20 22:26:58 +0700

    Add files via upload

---
+++ bt2ss2.c
@@ -0,0 +1,13 @@
+int main() {
+        // Khai bao va khoi tao hai bien so nguyen
+    int num1 = 10; // Bien so nguyen thu nhat
+    int num2 = 20; // Bien so nguyen thu hai
+
+    // Tinh tong hai bien va luu vao mot bien khac
+    int sum = num1 + num2; // Tong cua num1 va num2
+
+    // In ket qua
+    printf("Tong cua %d va %d la: %d\n", num1, num2, sum);
+
+    return 0;
+}
